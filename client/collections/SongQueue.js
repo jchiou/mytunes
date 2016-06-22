f4ed289045bac027aa3baba34d2453d9e1ab2250
jchiou <jessica.chiou@gmail.com>
2016-06-22 15:46:54 -0700

    Add a song to the queue and play if necessary Remove song when song ends Remove song when song is dequeued

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -4,6 +4,28 @@ var SongQueue = Backbone.Collection.extend({
   model: SongModel,
 
   initialize: function() {
-  }
+    this.on('enqueue', function() {
+      if (this.length === 1) {
+        this.playFirst();
+      }
+    }, this);
+
+    this.on('ended', function(song) {
+      this.remove(song);
+      if (this.length > 0) {
+        this.playFirst();
+      }
+    });
+
+    this.on('dequeue', function(song) {
+      this.remove(song);
+    });
+  },
+
+  playFirst: function() {
+    this.at(0).play();
+  },
+
+
 
 });
