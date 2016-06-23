e06cdf173376903069a197afbe7453c8f3b9a134
jchiou <jessica.chiou@gmail.com>
2016-06-22 17:17:46 -0700

    Change test

--- test/spec/SongQueueViewSpec.js
+++ test/spec/SongQueueViewSpec.js
@@ -3,34 +3,34 @@ describe('SongQueueView', function() {
 
   beforeEach(function() {
     fakeSongs = new SongQueue([
-      {
+      new SongModel({
         artist: 'data',
         url: '/test/testsong.mp3',
         title: 'test song'
-      },
-      {
+      }),
+      new SongModel({
         artist: 'data',
         url: '/test/testsong2.mp3',
         title: 'test song 2'
-      }
+      })
     ]);
   });
 
-  xit('creates SongQueueEntryViews for each queued song & renders them', function() {
+  it('creates SongQueueEntryViews for each queued song & renders them', function() {
     sinon.spy(SongQueueEntryView.prototype, 'render');
     view = new SongQueueView({collection: fakeSongs});
     view.render();
     expect(SongQueueEntryView.prototype.render).to.have.been.called;
   });
 
-  xit('renders when add or remove event fires from the song queue collection', function() {
+  it('renders when add or remove event fires from the song queue collection', function() {
     sinon.spy(SongQueueView.prototype, 'render');
     view = new SongQueueView({collection: fakeSongs});
-    view.collection.add({
+    view.collection.add(new SongModel({
       artist: 'data',
       url: '/test/testsong3.mp3',
       title: 'test song 3'
-    });
+    }));
     view.collection.pop();
     expect(view.render).to.have.been.called;
   });
