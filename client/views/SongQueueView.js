29e8c8a2280171c10257fdccd9a360561e5b495d
jchiou <jessica.chiou@gmail.com>
2016-06-22 17:16:15 -0700

    Pass spec for displaying SongQueue when it is updated

--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -1,12 +1,18 @@
 // SongQueueView.js - Defines a backbone view class for the song queue.
 var SongQueueView = Backbone.View.extend({
+  tagName: 'table',
 
   initialize: function() {
+    this.listenTo(this.collection, 'add', this.render);
+    this.listenTo(this.collection, 'remove', this.render);
   },
 
   render: function() {
+    this.collection.forEach(function(song) {
+      var songView = new SongQueueEntryView({model: song});
+      this.$el.append(songView.render());
+    }, this);
   },
-  initialize: function() {
-  }
+
 
 });
