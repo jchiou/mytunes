63297fbd656991ead5a31c2032625fab4ad9f821
jchiou <jessica.chiou@gmail.com>
2016-06-22 20:18:47 -0700

    Check when song is finished playing and broadcast event

--- client/views/PlayerView.js
+++ client/views/PlayerView.js
@@ -6,10 +6,14 @@ var PlayerView = Backbone.View.extend({
   el: '<audio controls autoplay />',
 
   initialize: function() {
+    
   },
 
   setSong: function(song) {
     this.model = song;
+    this.$el.on('ended', function() {
+      song.ended();
+    });
     this.render();
   },
 
