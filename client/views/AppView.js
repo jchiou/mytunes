35609f15236dcbeaedd8d01fcf5210c6f77529b2
jchiou <jessica.chiou@gmail.com>
2016-06-23 12:06:12 -0700

    Remove search DOM element creation

--- client/views/AppView.js
+++ client/views/AppView.js
@@ -5,6 +5,7 @@ var AppView = Backbone.View.extend({
     this.playerView = new PlayerView({model: this.model.get('currentSong')});
     this.libraryView = new LibraryView({collection: this.model.get('library')});
     this.queueView = new SongQueueView({collection: this.model.get('songQueue')});
+    this.searchView = new SearchView();
     // change:currentSong - this is Backbone's way of allowing you to filter events to
     // ONLY receive change events for the specific property, 'currentSong'
     this.model.on('change:currentSong', function(model) {
@@ -13,22 +14,9 @@ var AppView = Backbone.View.extend({
   },
 
   render: function() {
-    var searchBar = '<input type="search" placeholder="Search" class="searchbar"></input>';
-    var searchTypeList = [
-      '<select>',
-        '<option value="Artist">Artist</option>',
-        '<option value="Title">Title</option>',
-      '</select>'].join('');
-    var searchSection = [
-      '<div class="search">',
-        searchTypeList,
-        searchBar,
-      '</div>'
-    ].join('');
-
     return this.$el.html([
       this.playerView.$el,
-      searchSection,
+      this.searchView.$el,
       this.libraryView.$el,
       this.queueView.$el
     ]);
