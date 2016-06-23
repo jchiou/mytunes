b3cbe4c04099e27b2bc6df89f3d4675cf7149009
jchiou <jessica.chiou@gmail.com>
2016-06-23 10:27:16 -0700

    Fetch songs from server on initialize

--- client/collections/Songs.js
+++ client/collections/Songs.js
@@ -1,6 +1,24 @@
 // Songs.js - Defines a backbone collection class for songs.
 var Songs = Backbone.Collection.extend({
 
-  model: SongModel
+  model: SongModel,
+
+  initialize: function() {
+    var collection = this;
+    $.ajax({
+      url: 'https://api.parse.com/1/classes/songs/',
+      type: 'GET',
+      success: function (result) {
+        result.results.forEach(function(song) {
+          collection.add(song);
+        });
+      },
+      error: function (data) {
+        console.error('Failed to fetch');
+      }
+    });
+
+
+  }
 
 });
